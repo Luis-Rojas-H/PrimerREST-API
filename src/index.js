c13bbd8bdb210749ec0a508dca6c5f7f18a4eb04
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-09-18 21:13:09 -0500

    iniciando morgan, express.json and express.urlencode
    el formato 'dev' de morgan no ayuda  a ver la informacion que solicita o realiza el navegador y los express para ver info json y la de los url para ver inputs de formularios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
+// middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// empezando el servido
 app.listen(3000, () => {
-    console.log('Server on port ${3000}')
+    console.log(`Server on port ${3000}`)
 })
