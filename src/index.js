d48e10eee3e5a460895dd9fa12bb967593ff2e25
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-09-18 21:21:15 -0500

    configuración del servidor
    se usa app.set para definir el puerto, en caso no halla uno definido se usara el puerto 3000

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+// settings
+app.set('port', 3000)
+
 // middlewares
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false     }))
 app.use(express.json())
 
 // empezando el servido
-app.listen(3000, () => {
-    console.log(`Server on port ${3000}`)
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
 })
