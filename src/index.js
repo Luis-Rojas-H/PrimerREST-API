a8d59d489c09c9b7a1b2877b24b276f05c866f87
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-09-21 10:34:58 -0500

    terminado 1.0
    falta actualizar node-fetch
    mejorar route.delete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+var bodyParser = require('body-parser');// instalar: npm install body-parser
+
+//Parsear el body usando body parser
+app.use(bodyParser.json()); // body en formato json
+app.use(bodyParser.urlencoded({ extended: false })); //body formulario
 
 // settings
 app.set('port', process.env.PORT || 3000)
@@ -9,6 +14,8 @@ app.set('json spaces', 2)
 // routes
 app.use(require('./routes/index'))
 app.use('/api/movies', require('./routes/movies'))
+app.use('/api/users', require('./routes/users'))
+
 
 // middlewares
 app.use(morgan('dev'))
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,9 +1,55 @@
 const { Router } = require('express')
-const router = Router()
+const router = new Router()
+const _ = require('underscore')
 const movies = require('../sample.json')
 
 router.get('/', (req, res) => {
     res.json(movies)
 })
+
+router.post('/', (req, res) => {
+    const { title, year, rating, director } = req.body
+    if ( title && year && rating && director ) {
+        const id = movies.length + 1
+        const newMovie = {id, ...req.body}
+        movies.push(newMovie)
+        
+    }
+    else {
+        res.status(500).json({error: "hubo en grave error"})
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { title, year, rating, director } = req.body
+    if ( title && year && rating && director ) {
+        _.each(movies, (movie, i) => {
+            if(movie.id == id){
+                movie.title = title
+                movie.director = director
+                movie.year = year
+                movie.rating = rating
+            }
+        })
+        res.json(movies)
+    } else {
+        res.status(500).json({error: "hubo en grave error"})
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    if ( id ) {
+        _.each(movies, (movie, i) => {
+            if(movie.id == id){
+                movies.splice(i, 1);
+            }
+        })
+        res.json(movies)
+    } else {
+        res.status(500).json({error: "hubo en grave error"})
+    }  
+});
 
 module.exports = router
